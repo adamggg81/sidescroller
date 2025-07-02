adc5101f5e86f290054d394a461d47dde67ff0bd
Adam Gregory <adam.gregory@samtec.com>
2025-07-02 08:13:51 -0400

    Player and Frog are part of Character class (will eventually make Frog part of Enemy class)

---
+++ src/Character.py
@@ -0,0 +1,82 @@
+import pygame
+import globals as GLOBAL
+import Geometry as Geometry
+
+
+class Character:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 0
+        self.height = 0
+        self.vel_x = 0
+        self.vel_y = 0
+        self.speed = 0
+        self.jump_power = 0
+        self.jumping = False
+        self.gravity = GLOBAL.gravity
+        self.on_ground = False
+        self.floor_kills = True
+        self.alive = True
+        self.initial_x = x
+        self.initial_y = y
+        self.image_list = []
+        self.image = None
+        self.rect = None
+
+    def update(self, platforms):
+        original_ground_status = self.on_ground
+        original_y = self.y
+        # Apply gravity
+        self.vel_y += self.gravity
+
+        # Update position
+        self.x += self.vel_x
+        self.y += self.vel_y
+
+        self.on_ground = False
+
+        # Ground collision (simple floor at bottom of screen)
+        if self.y + self.height >= GLOBAL.WORLD_HEIGHT - GLOBAL.GROUND_HEIGHT:
+            self.y = GLOBAL.WORLD_HEIGHT - GLOBAL.GROUND_HEIGHT - self.height
+            # self.vel_y = 0
+            self.on_ground = True
+
+            # check if floor kills
+            if self.floor_kills:
+                self.alive = False
+
+        [is_on_platform, target_platform] = Geometry.platform_collision(self, platforms)
+        if is_on_platform:
+            self.y = target_platform.rect.y - self.height
+            self.on_ground = True
+
+        if self.on_ground:
+            self.vel_y = 0
+        if original_ground_status and self.on_ground:
+            self.y = original_y
+
+        # Keep player within world boundaries
+        if self.x < 0:
+            self.x = 0
+        elif self.x + self.width > GLOBAL.WORLD_WIDTH:
+            self.x = GLOBAL.WORLD_WIDTH - self.width
+
+        if not self.alive:
+            self.die()
+
+    def die(self):
+        pass
+
+    def on_screen(self, camera):
+        result = False
+        in_x = False
+        in_y = False
+        if self.x+self.width > camera.x and self.x < camera.x + GLOBAL.SCREEN_WIDTH:
+            in_x = True
+        if self.y+self.height > camera.y and self.y < camera.y + GLOBAL.SCREEN_HEIGHT:
+            in_y = True
+        if in_x and in_y:
+            result = True
+
+        return result
--- src/Frog.py
+++ src/Frog.py
@@ -2,25 +2,30 @@ import pygame
 import math
 import Geometry as Geometry
 import globals as GLOBAL
+from Character import Character
 
 
-class Frog:
+class Frog(Character):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+
+        # call init for Character
+        super().__init__(x, y)
+
+        # self.x = x
+        # self.y = y
         self.width = 40
         self.height = 40
-        self.vel_x = 0
-        self.vel_y = 0
+        # self.vel_x = 0
+        # self.vel_y = 0
         self.speed = 5
-        self.jump_power = -15
+        # self.jump_power = -15
         self.jumping = False
-        self.gravity = 0.8
-        self.on_ground = False
-        self.alive = True
-        self.initial_x = x
-        self.initial_y = y
-        self.image_list = []
+        # self.gravity = 0.8
+        # self.on_ground = False
+        # self.alive = True
+        # self.initial_x = x
+        # self.initial_y = y
+        # self.image_list = []
 
         # self.image = pygame.image.load("player.png").convert_alpha()
         image_list = ["frog.png"]
@@ -39,42 +44,43 @@ class Frog:
         self.rect.center = (screen_x + self.width / 2, screen_y + self.height / 2)
         screen.blit(self.image, self.rect)
 
-    def update(self, platforms):
-        original_ground_status = self.on_ground
-        original_y = self.y
-        # Apply gravity
-        self.vel_y += self.gravity
+    # def update(self, platforms):
+    #     original_ground_status = self.on_ground
+    #     original_y = self.y
+    #     # Apply gravity
+    #     self.vel_y += self.gravity
+    #
+    #     # Update position
+    #     self.x += self.vel_x
+    #     self.y += self.vel_y
+    #
+    #     self.on_ground = False
+    #
+    #     # Ground collision (simple floor at bottom of screen)
+    #     if self.y + self.height >= GLOBAL.WORLD_HEIGHT - GLOBAL.GROUND_HEIGHT:
+    #         self.y = GLOBAL.WORLD_HEIGHT - GLOBAL.GROUND_HEIGHT - self.height
+    #         # self.vel_y = 0
+    #         self.on_ground = True
+    #
+    #         # test making ground = death
+    #         # self.alive = False
+    #
+    #     [is_on_platform, target_platform] = Geometry.platform_collision(self, platforms)
+    #     if is_on_platform:
+    #         self.y = target_platform.rect.y - self.height
+    #         self.on_ground = True
+    #
+    #     if self.on_ground:
+    #         self.vel_y = 0
+    #     if original_ground_status and self.on_ground:
+    #         self.y = original_y
+    #
+    #     # Keep player within world boundaries
+    #     if self.x < 0:
+    #         self.x = 0
+    #     elif self.x + self.width > GLOBAL.WORLD_WIDTH:
+    #         self.x = GLOBAL.WORLD_WIDTH - self.width
+    #
+    #     # if not self.alive:
+    #     #     self.reset()
 
-        # Update position
-        self.x += self.vel_x
-        self.y += self.vel_y
-
-        self.on_ground = False
-
-        # Ground collision (simple floor at bottom of screen)
-        if self.y + self.height >= GLOBAL.WORLD_HEIGHT - GLOBAL.GROUND_HEIGHT:
-            self.y = GLOBAL.WORLD_HEIGHT - GLOBAL.GROUND_HEIGHT - self.height
-            # self.vel_y = 0
-            self.on_ground = True
-
-            # test making ground = death
-            # self.alive = False
-
-        [is_on_platform, target_platform] = Geometry.platform_collision(self, platforms)
-        if is_on_platform:
-            self.y = target_platform.rect.y - self.height
-            self.on_ground = True
-
-        if self.on_ground:
-            self.vel_y = 0
-        if original_ground_status and self.on_ground:
-            self.y = original_y
-
-        # Keep player within world boundaries
-        if self.x < 0:
-            self.x = 0
-        elif self.x + self.width > GLOBAL.WORLD_WIDTH:
-            self.x = GLOBAL.WORLD_WIDTH - self.width
-
-        # if not self.alive:
-        #     self.reset()
